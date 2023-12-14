a8809c42ef1826ba47d6414baeb37ffbc757de50
NirodhaXN <153802716+NirodhaXN@users.noreply.github.com>
2023-12-14 12:48:15 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+turtle.setPosition(2, 2)
+turtle.pen(TurtlePenMode.Down)
+for (let index = 0; index < 2; index++) {
+    turtle.turnRight()
+    turtle.forward(1)
+}
+for (let index = 0; index < 2; index++) {
+    turtle.turnRight()
+    turtle.forward(2)
+}
+for (let index = 0; index < 2; index++) {
+    turtle.turnRight()
+    turtle.forward(3)
+}
